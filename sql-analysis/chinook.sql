14a3be6cdf486ff673c3cf619fe4f77a2d66d8d1
JHarditech <james_hardiman@outlook.com>
2025-10-07 17:57:52 +0100

    Day 2

--- sql-analysis/chinook.sql
+++ sql-analysis/chinook.sql
@@ -44,10 +44,61 @@ SELECT COUNT(*) FROM InvoiceLine
 SELECT COUNT(*) FROM InvoiceLine 
     WHERE Quantity <= 0;
 
-
 -- Checking for duplicate IDs
+SELECT AlbumId, COUNT(*) AS count
+    FROM Album
+    GROUP BY AlbumId
+    HAVING COUNT(*) > 1;
+SELECT ArtistId, COUNT(*)
+    FROM Artist
+    GROUP BY ArtistId
+    HAVING COUNT(*) > 1;
+SELECT CustomerId, COUNT(*) AS count 
+    FROM Customer 
+    GROUP BY CustomerId
+    HAVING COUNT(*) > 1;
+SELECT EmployeeId, COUNT(*) AS count
+    FROM Employee
+    GROUP BY EmployeeId
+    HAVING COUNT(*) > 1;
+SELECT GenreId, COUNT(*) AS count 
+    FROM Genre
+    GROUP BY GenreId
+    HAVING COUNT(*) > 1;
 SELECT InvoiceId, COUNT(*) AS count
     FROM Invoice
     GROUP BY InvoiceId
-    HAVING COUNT(*) >= 1
+    HAVING COUNT(*) > 1;
+SELECT InvoiceLineId, COUNT(*) AS count
+    FROM InvoiceLine
+    GROUP BY InvoiceLineId
+    HAVING COUNT(*) > 1;
+SELECT MediaTypeId, COUNT(*) AS count 
+    FROM MediaType
+    GROUP BY MediaTypeId
+    HAVING COUNT(*) > 1;
+SELECT PlaylistId, Count(*) AS count 
+    FROM Playlist
+    GROUP BY PlaylistId
+    HAVING COUNT(*) > 1;
+SELECT TrackID, COUNT(*) AS count 
+    FROM Track
+    GROUP BY TrackId
+    HAVING COUNT(*) > 1;
+
+
+-- Calculating Total Revenue
+SELECT SUM(Total)
+    FROM Invoice
+SELECT SUM(UnitPrice * Quantity)
+    FROM InvoiceLine
+
+--Example JOIN - not needed
+SELECT invoice.Total AS total, 
+    invoice_line.UnitPrice AS unit_price,
+    invoice_line.Quantity AS quantity, 
+    (invoice_line.UnitPrice * invoice_line.Quantity) AS revenue
+    FROM Invoice AS invoice
+    JOIN InvoiceLine AS invoice_line
+    ON invoice.InvoiceId = invoice_line.InvoiceId;
 
